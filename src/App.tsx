4a42652821f82a3140d05648b206806e1291177e
aexiting <adamttway@gmail.com>
2025-04-20 09:46:02 -0400

    added some error handling for max length and empty values

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import './App.css'
 import QRFactory from 'qrcode-generator';
 
+type Error = {
+    hasError: boolean;
+    errorInfo: 'EMPTY_STRING' | 'MAX_LENGTH' |  null;
+}
+const initialError = { hasError: false, errorInfo: null }
 function App() {
-    const ERROR_CORRECTION_LEVEL = 'M';
+    const ERROR_CORRECTION_LEVEL = 'L';
     const TYPE_NUMBER = 0;
-
+    const MAX_LENGTH = 20;
     const [input, setInput] = useState('')
     const [qrcode, setQRCode] = useState('')
     const [history, setHistory] = useState<string[]>([])
+    const [error, setError] = useState<Error>(initialError)
 
     const qr = QRFactory(TYPE_NUMBER, ERROR_CORRECTION_LEVEL)
 
@@ -16,26 +22,39 @@ function App() {
         setInput(text)
     }
 
-    const generateQRCode = () => {
-        if(!input) {
-            return;
+    const updateQRCode = (data: string, updateHistory: boolean = false) => {
+        if (!data) {
+            setError({hasError: true, errorInfo: 'EMPTY_STRING'})
         }
-        qr.addData(input)
-        qr.make()
-        setQRCode(qr.createImgTag(10))
-        setHistory([...history, input])
+        else if (data.length > MAX_LENGTH) {
+            setError({hasError: true, errorInfo: 'MAX_LENGTH'})
+        }
+        else {
+            setError(initialError)
+            qr.addData(data);
+            qr.make();
+            setQRCode(qr.createImgTag(10))
+            if(updateHistory){
+                setHistory([...history, input])
+            }
+        }
+    }
+
+    const generateQRCode = () => {
+        updateQRCode(input, true)
         setInput('')
     }
+
     const generateFromListItem = (value :string) => {
-        qr.addData(value)
-        qr.make();
-        setQRCode(qr.createImgTag(10))
+        updateQRCode(value)
         setInput(value)
     }
 
     return (
     <>
         <div id="input-container">
+            {error.hasError && error.errorInfo == 'EMPTY_STRING' && <span className="error" >QRCode input is empty...</span>}
+            {error.hasError && error.errorInfo == 'MAX_LENGTH' && <span className="error" >QRCode input is too long...</span>}
             <h1 id="title">QRCode Generator</h1>
             <input
                 type="text"
@@ -52,8 +71,8 @@ function App() {
             <h1 id="title">Previous Codes</h1>
             <ul className="history-list">
                 {
-                    history.map(historyItem =>
-                        <li className="history-item">
+                    history.map((historyItem, index) =>
+                        <li key={index} className="history-item">
                             <button onClick={() => generateFromListItem(historyItem)}>{historyItem}</button>
                         </li>
 
