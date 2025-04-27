0997a60f0f708acb51553bbc137818312a6ad91f
aexiting <adamttway@gmail.com>
2025-04-27 07:52:04 -0400

    Tried testing the share button and I realized that just clicking the image much faster than having it's own button for it.

--- src/components/QrCodeGen.tsx
+++ src/components/QrCodeGen.tsx
@@ -12,46 +12,40 @@ export const QrCodeGen = (
 
     return (
         <>
-            <div id="input-container">
-                {qrCodeGenState.error.hasError && qrCodeGenState.error.errorInfo == 'EMPTY_STRING' &&
-                    <span className="error">QRCode input is empty...</span>}
-                {qrCodeGenState.error.hasError && qrCodeGenState.error.errorInfo == 'MAX_LENGTH' &&
-                    <span className="error">QRCode input is too long...</span>}
-                <h1 className="title">QRCode Generator</h1>
-                <input
-                    type="text"
-                    id="text-input"
-                    name="text-input"
-                    onChange={(e) => qrCodeGenActions.setInput(e.target.value)}
-                    value={qrCodeGenState.input}
-                    placeholder="Enter info here">
-                </input>
-                <button className="button-64" role="button" onClick={() => qrCodeGenActions.generateQRCode()}>
-                    <span className="text">Generate</span>
-                </button>
-                {qrCodeGenState.qrcode &&
-                    <div className="qr-code" dangerouslySetInnerHTML={{__html: qrCodeGenState.qrcode}}/>}
-                { navigator.canShare() && <button onClick={() => navigator.share(
-                    {
-                        title: "QR-Code",
-                        text: "Share QR-Code as needed.",
-                        url: qrCodeGenState.qrcode
-                    }
-                )}>Share</button>}
+        <div id="input-container">
+            {qrCodeGenState.error.hasError && qrCodeGenState.error.errorInfo == 'EMPTY_STRING' &&
+                <span className="error">QRCode input is empty...</span>}
+            {qrCodeGenState.error.hasError && qrCodeGenState.error.errorInfo == 'MAX_LENGTH' &&
+                <span className="error">QRCode input is too long...</span>}
+            <h1 className="title">QRCode Generator</h1>
+            <input
+                type="text"
+                id="text-input"
+                name="text-input"
+                onChange={(e) => qrCodeGenActions.setInput(e.target.value)}
+                value={qrCodeGenState.input}
+                placeholder="Enter info here">
+            </input>
+            <button className="button-64" role="button" onClick={() => qrCodeGenActions.generateQRCode()}>
+                <span className="text">Generate</span>
+            </button>
+            {qrCodeGenState.qrcode &&
+                <div className="qr-code" dangerouslySetInnerHTML={{__html: qrCodeGenState.qrcode}}/>}
                 <h1 className="title">Previous Codes</h1>
                 <ul className="history-list">
-                    {
-                        qrCodeGenState.history.map((historyItem: string, index: number) =>
-                            <li key={index} className="history-item" draggable>
-                                <button
-                                    onClick={() => qrCodeGenActions.generateFromListItem(historyItem)}>{historyItem}</button>
-                                <button className="delete-button"
-                                    onClick={() => qrCodeGenActions.deleteHistoryItem(historyItem)}>Delete</button>
-                            </li>
-                        )
-                    }
-                </ul>
-            </div>
+            {
+                qrCodeGenState.history.map((historyItem: string, index: number) =>
+                <li key={index} className="history-item" draggable>
+                <button
+                    onClick={() => qrCodeGenActions.generateFromListItem(historyItem)}>{historyItem}</button>
+                <button className="delete-button"
+                        onClick={() => qrCodeGenActions.deleteHistoryItem(historyItem)}>Delete
+                </button>
+            </li>
+                )
+            }
+        </ul>
+        </div>
         </>
-    )
+)
 }
